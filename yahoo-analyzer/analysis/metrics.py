01da80222d147b3b0426b76107a5f5a7c32d80ec
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2022-06-23 03:00:08 +0200

    Working on it

---
+++ yahoo-analyzer/analysis/metrics.py
@@ -0,0 +1,14 @@
+class StockMetrics:
+    """
+    Parameters:
+    ROI: Return on Investment
+    EPS: Earnings per Share
+    PER: Price to Earnings
+    """
+
+    def __init__(self):
+        self.return_on_investment = 0
+        self.earnings_per_share = 0
+        self.price_to_earnings_ratio = 0
+        self.return_on_equity = 0
+        self.compound_annual_growth_rate = 0
--- yahoo-analyzer/get_data.py
+++ yahoo-analyzer/get_data.py
@@ -1,8 +1,27 @@
+from datetime import date
+
 import yfinance as yf
 
+
+def get_change_between_dates(
+    tickers: list[str],
+    start: date = None,
+    end: date = None,
+) -> float:
+    days_between_dates = (end - start).days
+    interval =
+    print(type(time_difference))
+    data = yf.download(
+        tickers=tickers,
+        start=start,
+        end=end,
+    )
+    return data
+
+
 if __name__ == "__main__":
-    novo_nordisk = yf.Ticker("NVO")
+    start = date(2017, 11, 1)
+    end = date.today()
+    data = get_change_between_dates(tickers=["NVO", "DANSKE.CO"], start=start, end=end)
 
-    history = novo_nordisk.history()
-
-    print(history)
+    print(data.info)
